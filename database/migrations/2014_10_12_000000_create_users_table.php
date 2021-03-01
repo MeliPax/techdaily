3bb8a8cf813d490d501496e63715352328432f73
MeliPax <33260323+MeliPax@users.noreply.github.com>
2021-03-01 09:25:50 +0200

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->string('role')->deafault('2');
+            $table->string('role');
             $table->string('status')->default('0');
         });
     }
